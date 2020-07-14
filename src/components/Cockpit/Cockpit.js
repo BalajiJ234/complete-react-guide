53995f7aa3ccf0d61d3ac37d1e2a65b544ebb384
Balaji Janarthanan <31264281+BalajiJ234@users.noreply.github.com>
2020-07-14 21:03:59 +0530

    Using useEffect() in Functional Components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import classes from "./Cockpit.css"
+import classes from "./Cockpit.css";
 
-const Cockpit = ( props ) => {
+const Cockpit = (props) => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    //Http Request...
+    setTimeout(() => {
+      alert('Saved data to cloud!');
+    }, 1000)
+  }, []);
+
   const Assignedclasses = [];
-  let btnClass = '';
-  
-  if(props.showPersons) {
+  let btnClass = "";
+
+  if (props.showPersons) {
     btnClass = classes.Red;
   }
+
   if (props.persons.length <= 2) {
     Assignedclasses.push(classes.red);
   }
@@ -20,7 +29,9 @@ const Cockpit = ( props ) => {
   return (
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
-      <p className={Assignedclasses.join(" ")}>This is really working! demo! hurray!</p>
+      <p className={Assignedclasses.join(" ")}>
+        This is really working! demo! hurray!
+      </p>
       <button className={btnClass} onClick={props.clicked}>
         Toggle Persons
       </button>
